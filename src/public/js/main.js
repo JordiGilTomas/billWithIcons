6d3e9dac8dc4662650fea5e33da2ae6d14e82ec5
JordiGilTomas <gil.tomas.jordi@gmail.com>
2020-03-18 14:13:44 +0100

    Added Shopping cart Added animation to show shopping cart

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,13 +1,22 @@
 const nombreInput = document.getElementById('nombreInput');
-const tableProductos = document.querySelectorAll('.tableProductos tbody tr');
+const tableRows = document.querySelectorAll('.tableProductos tbody tr');
 const table = document.getElementById('tableProductos');
+const carrito = document.getElementById('cartCantidad');
 
 const showProducts = (e) => {
     const nombre = (e.target.value).toLowerCase();
-    tableProductos.forEach((item) => ((((item.querySelectorAll('td:nth-child(2)')[0].textContent).toLowerCase().includes(nombre) && nombre) || nombre === '*')
-            ? item.classList.remove('hidden')
-            : item.classList.add('hidden')));
+    // nth-child(2) es la celda del nombre del producto
+    // Vemos si incluye nombre, comprobamos si nombre no es null y comprobamos si es *
+    // si es alguno de estos casos quitamos la clase hidden para mostrarlos.
+    tableRows.forEach((row) => ((((row.querySelectorAll('td:nth-child(2)')[0].textContent).toLowerCase().includes(nombre) && nombre) || nombre === '*')
+        ? row.classList.remove('hidden')
+        : row.classList.add('hidden')));
 };
 
+const addProductToCart = (e) => {
+    if (e.target.tagName === 'BUTTON') carrito.textContent = parseInt(carrito.textContent, 10) + 1;
+};
+
+
 nombreInput.addEventListener('keyup', showProducts);
-table.addEventListener('click', (e) => console.log(e.target));
+table.addEventListener('click', addProductToCart);
